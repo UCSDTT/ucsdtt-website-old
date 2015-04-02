1acfe81b895e509ac1f40bc4cee429e27378f9c9
Michael Luo <miczluo@gmail.com>
2015-04-02 04:38:12 -0700

    added twitter quotes

--- app.js
+++ app.js
@@ -38,10 +38,29 @@ app.use(function(req, res, next) {
     next(err);
 });
 
+
+
 // set port to number provided from user or defaults to 3000
 app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function() {
+  // implementation of twitter stream on website
+  var io = require('socket.io')(server);
+  var Twitter = require('node-tweet-stream'),
+    tw = new Twitter({
+      consumer_key: 'Aah1DZDZAHJcvguh6mpymarYl',
+      consumer_secret: 'QALUC2QHUQJj2zZAF5Tr22ezqyzz673XG3z0gmXcDQcq3zUeNj',
+      token: '2983641870-TsjqyWFE2Q6VCp0E8F3IN4tHZ1tzPIxhMAksZ0w',
+      token_secret: 'nBGdIfQ2tQl9aU726M109wJnFLxmqE1ck8RiwHs3qBYqL'
+    });
+
+  tw.track('3dprinting');
+  tw.track('virtualreality');
+
+  tw.on('tweet', function(tweet){
+    io.emit('tweet', tweet);
+    console.log(tweet);
+  });
   console.log('Express server listening on port ' + server.address().port);
 });
 
